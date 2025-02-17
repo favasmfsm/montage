ee55641c6b070bac7e7a84f4213ac6ac39a586a3
Favas M <favasmfsm@gmail.com>
2025-02-17 12:23:20 +0530

    print tot screen

--- src/streamlit_v5.py
+++ src/streamlit_v5.py
@@ -548,7 +548,7 @@ def fetch_car_data(
     if radius:
         params["radius"] = radius
     params["msrp_range"] = msrp_range
-    print(params)
+    st.write(params)
     # st.write(params, selected_config)
     response = requests.get(BASE_URL, params=params)
 
